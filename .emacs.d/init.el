751233f5d0e0cb145a0391752a5fa1ac1cea546b
shuta <toda.shuta@gmail.com>
2012-05-29 00:15:31 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 ;; emacs settings
 
-;; == Window ==========================
+;; Window {{{ -------------------------
+
 (if window-system (progn
    (set-frame-parameter nil 'alpha '(82 55))  ;; 透明度(アクティブ 非アクティブ)
    (set-background-color "Black")  ;; 背景色
@@ -12,22 +13,30 @@
 ;   (set-default 'line-spacing 7)  ;; 表示の行間を拡げる
    ))
 
-;; == Window size =====================
-;; 初期フレームの設定
+;; }}}
+
+;; Window size {{{ --------------------
+
+;; 初期フレームの設定 {{{
 (setq initial-frame-alist
       '((width . 80)    ; フレーム幅(文字数)
         (height . 32)   ; フレーム高(文字数)
 ;        (top . 22)
 ;        (left . 3)
         ))
+;; }}}
 
-;; 新規フレームのデフォルト設定
+;; 新規フレームのデフォルト設定 {{{
 (setq default-frame-alist
       '((width . 80)    ; フレーム幅(文字数)
         (height . 24))  ; フレーム高(文字数)
         )
+;; }}}
 
-;; == For Mac (Carbon Emacs) ==========
+;; }}}
+
+;; For Mac (Carbon Emacs) {{{ ---------
+
 ;; フォントの変更 (fixed-width-fontset: carbon-font)
 ;; 使えるフォント："hiramaru" "hirakaku_w3" "hirakaku_w6" "hirakaku_w8" "hiramin_w3" "hiramin_w6" "osaka"
 ;; 使えるサイズ：7, 8, 9, 10, 12, 14, 16, 18, 20, 24
@@ -35,14 +44,20 @@
   (require 'carbon-font)
   (fixed-width-set-fontset "hiramaru" 14))
 
-;; その他Mac用設定
+;; }}}
+
+;; その他Mac用設定 {{{
+
 (when (eq window-system 'mac)
   (setq mac-allow-anti-aliasing t)  ;; Quartz 2D のアンチエイリアスを利用する
 ;  (mac-key-mode 1)  ;; Macのキーバインドを使う
 ;  (setq mac-option-modifier 'meta)  ;; optionをメタキーにする(通常はCommandキーorESCキー)
   )
 
-;; == フォント設定 for Ubuntu =========
+;; }}}
+
+;; フォント設定 for Ubuntu {{{ --------
+
 (when (eq system-type 'gnu/linux)
 (custom-set-faces
  '(default ((t (:inherit nil :stipple nil :background "Black"
@@ -50,7 +65,10 @@
   :overline nil :underline nil :slant normal :weight normal :height 120
   :width normal :foundry "unknown" :family "Ricty"))))))
 
-;; == For Windows =====================
+;; }}}
+
+;; For Windows {{{ --------------------
+
 (when (eq system-type 'windows-nt)
 (setq default-directory "C:/Users/SHU/")  ;; 初期ディレクトリの設定
 (custom-set-faces
@@ -59,7 +77,10 @@
   :overline nil :underline nil :slant normal :weight normal :height 143
   :width normal :foundry "outline" :family "Ricty"))))))
 
-;; == solarized =======================
+;; }}}
+
+;; solarized {{{ ----------------------
+
 ;; color-theme を使う方式
 ;(add-to-list 'load-path "~/.emacs.d/site-lisp")
 ;(require 'color-theme)  ;; color-theme呼び出し
@@ -69,4 +90,7 @@
 ;(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized")
 ;(enable-theme 'solarized-dark)
 
+;; }}}
+
+;; vim:foldmethod=marker
 ;; end  of file
