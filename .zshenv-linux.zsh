240127c513421c6d73d589997dada040c164e3b5
todashuta <toda.shuta@gmail.com>
2017-03-15 00:40:25 +0900

    Style change

--- .zshenv-linux.zsh
+++ .zshenv-linux.zsh
@@ -1,22 +1,22 @@
 ## zshenv for Linux
 
-export ZDOTDIR="${HOME}/.zsh"
+export ZDOTDIR="$HOME/.zsh"
 
 export GOROOT="$HOME/local/go/current"  # official binary
 export GOPATH="$HOME/go"
 
 typeset -gx -U path
 path=(
-    ${HOME}/bin(N-/^W)
-    ${HOME}/.plenv/shims(N-/^W)
-    ${HOME}/.plenv/bin(N-/^W)
-    ${HOME}/.rbenv/bin(N-/^W)
-    ${HOME}/.cargo/bin(N-/^W)
-    ${path}
-    ${GOROOT}/bin(N-/^W)
-    ${GOPATH}/bin(N-/^W)
-    ${HOME}/local/bin(N-/^W)
-    ${HOME}/.local/bin(N-/^W)
+    $HOME/bin(N-/^W)
+    $HOME/.plenv/shims(N-/^W)
+    $HOME/.plenv/bin(N-/^W)
+    $HOME/.rbenv/bin(N-/^W)
+    $HOME/.cargo/bin(N-/^W)
+    $path
+    $GOROOT/bin(N-/^W)
+    $GOPATH/bin(N-/^W)
+    $HOME/local/bin(N-/^W)
+    $HOME/.local/bin(N-/^W)
 
     #/usr/local/sbin
     #/usr/sbin
@@ -28,7 +28,7 @@ export EDITOR=vim
 
 export LSCOLORS='gxfxcxdxbxegedabagacad'
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=36;40:tw=30;42:ow=30;43'
-export ZLS_COLORS="${LS_COLORS}"
+export ZLS_COLORS="$LS_COLORS"
 
 (( $+commands[rbenv] )) && eval "$(rbenv init - zsh)" || :
 #(( $+commands[plenv] )) && eval "$(plenv init - zsh)" || :
