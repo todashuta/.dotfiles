1c7c0b255dd3a8743c54755289161b89020c0bba
todashuta <toda.shuta@gmail.com>
2022-11-27 11:58:21 +0900

    update

--- .vim/template/python/snip-outputDebugString.py
+++ .vim/template/python/snip-outputDebugString.py
@@ -1,8 +1,10 @@
 def outputDebugString(obj):
+    prefix = ""
+    #import inspect; cf = inspect.currentframe(); prefix = f"{cf.f_back.f_code.co_filename}:{cf.f_back.f_lineno}: "
     import ctypes
     W32OutputDebugString = ctypes.windll.kernel32.OutputDebugStringW
     from pprint import pformat, pprint
     #pprint(obj)
-    return W32OutputDebugString(pformat(obj))
+    return W32OutputDebugString(prefix+pformat(obj))
 
 outputDebugString({{_cursor_}}
