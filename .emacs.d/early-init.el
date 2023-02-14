0cc1e93c78a0436ad857d503edff8aff1171376f
todashuta <toda.shuta@gmail.com>
2023-02-15 04:07:28 +0900

    update

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,3 +1,26 @@
-(require 'org)
-(org-babel-load-file
- (expand-file-name (concat user-emacs-directory "my-early-init.org")))
+;; -*- lexical-binding: t; -*-
+
+(defconst my:saved-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq file-name-handler-alist my:saved-file-name-handler-alist)))
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq inhibit-redisplay t)
+(setq inhibit-message t)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq inhibit-redisplay nil)
+	    (setq inhibit-message nil)
+	    (redisplay)))
+
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+(provide 'early-init)
+;;; early-init.el ends here
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,3 @@
 (require 'org)
 (org-babel-load-file
- (expand-file-name (concat user-emacs-directory "my-init.org")))
+ (expand-file-name (concat user-emacs-directory "README.org")))
