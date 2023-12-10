2475ef46d3aa39e1e7ca0313560babc3b0f87ffc
todashuta <toda.shuta@gmail.com>
2023-12-10 20:52:24 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
 (require 'org)
-(org-babel-load-file
- (expand-file-name (concat user-emacs-directory "README.org")))
+(let* ((safe-local-variable-values '((org-src-preserve-indentation . t))))
+  (org-babel-load-file
+   (expand-file-name (concat user-emacs-directory "README.org"))))
