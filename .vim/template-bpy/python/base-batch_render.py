eead3602877843dd02a4c8f23a03ad86b2bc6eb2
todashuta <toda.shuta@gmail.com>
2024-11-04 22:11:03 +0900

    update

--- .vim/template-bpy/python/base-batch_render.py
+++ .vim/template-bpy/python/base-batch_render.py
@@ -1,21 +1,23 @@
 import bpy
 
-BODIES = ( "body-01", "body-02", "body-03" )
-WHEELS = ( "wheel-01", "wheel-02", "wheel-03" )
-WINGS  = ( "wing-01", "wing-02", "wing-03" )
-
 objects = bpy.data.objects
+
+BODIES = [ objects[ob] for ob in ("body-01", "body-02", "body-03") ]
+WHEELS = [ objects[ob] for ob in ("wheel-01", "wheel-02", "wheel-03") ]
+WINGS  = [ objects[ob] for ob in ("wing-01", "wing-02", "wing-03") ]
+
+ALL = BODIES + WHEELS + WINGS
 
 import itertools
 for body,wheel,wing in itertools.product(BODIES, WHEELS, WINGS):
-    for o in (BODIES + WHEELS + WINGS):
-        objects[o].hide_render = True
+    for o in ALL:
+        o.hide_render = True
 
-    objects[body].hide_render = False
-    objects[wheel].hide_render = False
-    objects[wing].hide_render = False
+    body.hide_render  = False
+    wheel.hide_render = False
+    wing.hide_render  = False
 
-    f = f"//render/{body}_{wheel}_{wing}"
+    f = f"//render/{body.name}_{wheel.name}_{wing.name}"
     bpy.context.scene.render.filepath = f
     bpy.ops.render.render(write_still=True)
     print(f)
