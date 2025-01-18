4c122a7b1a6b74774a6fb9a3d7edcbb61067bb3b
todashuta <toda.shuta@gmail.com>
2025-01-18 21:27:05 +0900

    update

--- .vim/denops/vimrc/main.ts
+++ .vim/denops/vimrc/main.ts
@@ -1,6 +1,8 @@
 import type { Entrypoint, Denops } from "jsr:@denops/std@^7.0.0";
 import { assert, is } from "jsr:@core/unknownutil@^4.0.0";
 
+import { format } from "npm:date-fns@4";
+
 export const main: Entrypoint = (denops: Denops) => {
   denops.dispatcher = {
     decodeURIComponent(s: unknown) {
@@ -10,6 +12,9 @@ export const main: Entrypoint = (denops: Denops) => {
     encodeURIComponent(s: unknown) {
       assert(s, is.String);
       return globalThis.encodeURIComponent(s);
+    },
+    dateIso8601() {
+      return format(new Date(), "yyyyMMdd'T'HHmmssXX");
     },
   };
 };
