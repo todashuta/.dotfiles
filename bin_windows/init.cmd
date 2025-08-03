c3bf8940c4e57e33264eb41ff2d40d84d851af8f
todashuta <toda.shuta@gmail.com>
2025-08-04 00:13:35 +0900

    update

--- bin_windows/init.cmd
+++ bin_windows/init.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-if "%CMD_INIT_SCRIPT_LOADED%" neq "" goto :EOF
-set CMD_INIT_SCRIPT_LOADED=yes
-
 doskey /macrofile=%~dp0.\init.macros
+
+if "%CMD_INIT_SCRIPT_LOADED%" neq "" goto :EOF
+set CMD_INIT_SCRIPT_LOADED=1
+
 ::set DISPLAY=localhost:0.0
 ::set HOME=%USERPROFILE%
 ::set EDITOR=gvim.exe
