0d146b27a114a7b566e4e755d1c33d3afe3bfe19
todashuta <toda.shuta@gmail.com>
2026-01-19 01:14:23 +0900

    update

--- .vim/templates/_/sh/base-wrapper.sh
+++ .vim/templates/_/sh/base-wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 installdir="/path/to/project/root/_install" # FIXME
+name="${0##*/}"
+
 export LD_LIBRARY_PATH="$installdir/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 export DYLD_LIBRARY_PATH="$installdir/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}" # macOS
 export PATH="$installdir/bin:$PATH"
-exec "$installdir/bin/command-name" "$@" # FIXME
+exec "$installdir/bin/$name" "$@"
