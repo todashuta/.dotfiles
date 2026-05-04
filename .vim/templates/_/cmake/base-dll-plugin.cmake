081fe93ac6df62eab88a10e2dca4d4e5a6156a9a
todashuta <toda.shuta@gmail.com>
2026-05-05 05:30:22 +0900

    update

---
+++ .vim/templates/_/cmake/base-dll-plugin.cmake
@@ -0,0 +1,31 @@
+cmake_minimum_required(VERSION 3.20)
+
+project(ExamplePlugin LANGUAGES C CXX)
+
+set(EXAMPLE_SDK_CPP_INCLUDE_DIR "C:/path/to/sdk/include")
+
+#add_compile_definitions(EXAMPLE_APP_PLUGIN=1)
+
+add_library(${CMAKE_PROJECT_NAME}
+	SHARED
+		#"${EXAMPLE_SDK_CPP_INCLUDE_DIR}/AWESOMEAPI.cpp"
+		src/plugin.cpp
+)
+
+#target_compile_features(${CMAKE_PROJECT_NAME} PRIVATE cxx_std_11)
+
+target_compile_options(${CMAKE_PROJECT_NAME}
+	PUBLIC
+		"$<$<C_COMPILER_ID:MSVC>:/utf-8>"
+		"$<$<CXX_COMPILER_ID:MSVC>:/utf-8>"
+)
+
+set_target_properties(${CMAKE_PROJECT_NAME}
+	PROPERTIES
+		PREFIX "" # 出力されるdllのファイル名先頭に付くlibを消す
+)
+
+target_include_directories(${CMAKE_PROJECT_NAME}
+	PUBLIC
+		"${EXAMPLE_SDK_CPP_INCLUDE_DIR}"
+)
